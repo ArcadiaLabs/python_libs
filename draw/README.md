9c1d79923ca8d33bb07525007fa17576696e638b
CaptainStouf <stouf@hardware-libre.org>
2016-02-16 15:33:33 +0100

    Added new functions to draw.drawfunctions.py See README
    Signed-off-by: CaptainStouf <stouf@hardware-libre.org>

---
+++ draw/README.md
@@ -0,0 +1,9 @@
+# Python Libraries
+
+###      Draw 
+##### drawfunctions
+For use with pygame, allows better placement of texts and pictures and allows text outline. 
+
+# 2016-02-16
+added draw_button(surface, img, position, text="none", font="none", color=(0, 255, 0), outline=True, outline_color=(1, 1, 1), align="none")
+added draw_roundrect(surface, color, rect, width, xr, yr)
--- draw/drawfunctions.py
+++ draw/drawfunctions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import python_libs.hollowtext as hollowtext
+import pygame
 
 def draw_text(surface, text, position, align="none", font="", color=(0, 255, 0), outline=True, outline_color=(1, 1, 1)):	
 	label = font.render(text, 1, color)
@@ -37,3 +38,53 @@ def draw_image(surface, img, position, align="none"):
 	elif align == "center":
 		x = x-(img.get_width()/2)
 	surface.blit(img, (x, y))
+	
+def draw_button(surface, img, position, text="none", font="none", color=(0, 255, 0), outline=True, outline_color=(1, 1, 1), align="none"):
+	x = position[0]
+	y = position[1]
+	if align == "right":
+		x = x-img.get_width()
+	elif align == "center":
+		x = x-(img.get_width()/2)
+	if text != "none":
+		label = font.render(text, 1, color)
+		label_position = ( x+(img.get_width()/2)-(label.get_width()/2), y+(img.get_height()/2)-(font.get_height()/2) )
+	surface.blit(img, (x, y))
+	
+	if outline == True:
+		text = hollowtext.textOutline(font, text, color, outline_color)
+		surface.blit( text, label_position)
+	else:
+		surface.blit( label, label_position)
+		
+#	surface.blit( label, (x+(img.get_width()/2)-(label.get_width()/2), y+(img.get_height()/2)-(font.get_height()/2) ))
+	
+def draw_roundrect(surface, color, rect, width, xr, yr): 
+	clip = surface.get_clip()
+	
+
+	# left and right
+	surface.set_clip(clip.clip(rect.inflate(0, -yr*2)))
+	pygame.draw.rect(surface, color, rect.inflate(1-width,0), width)
+
+	# top and bottom
+	surface.set_clip(clip.clip(rect.inflate(-xr*2, 0)))
+	pygame.draw.rect(surface, color, rect.inflate(0,1-width), width)
+
+	# top left corner
+	surface.set_clip(clip.clip(rect.left, rect.top, xr, yr))
+	pygame.draw.ellipse(surface, color, pygame.Rect(rect.left, rect.top, 2*xr, 2*yr), width)
+
+	# top right corner
+	surface.set_clip(clip.clip(rect.right-xr, rect.top, xr, yr))
+	pygame.draw.ellipse(surface, color, pygame.Rect(rect.right-2*xr, rect.top, 2*xr, 2*yr), width)
+
+	# bottom left
+	surface.set_clip(clip.clip(rect.left, rect.bottom-yr, xr, yr))
+	pygame.draw.ellipse(surface, color, pygame.Rect(rect.left, rect.bottom-2*yr, 2*xr, 2*yr), width)
+
+	# bottom right
+	surface.set_clip(clip.clip(rect.right-xr, rect.bottom-yr, xr, yr))
+	pygame.draw.ellipse(surface, color, pygame.Rect(rect.right-2*xr, rect.bottom-2*yr, 2*xr, 2*yr), width)
+
+	surface.set_clip(clip)
