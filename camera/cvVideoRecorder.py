2f427574509fe1eaa920f8d5ae9c644ff8669233
Stéphane Guerreau <contact@arcadia-labs.com>
2022-06-03 16:45:28 +0200

    Update cvVideoRecorder.py
    Corrected a bug where video was not captured after the first time

--- camera/cvVideoRecorder.py
+++ camera/cvVideoRecorder.py
@@ -9,19 +9,10 @@ from time import sleep
 class VideoRecorder:
     def __init__(self, res_x=640, res_y=480, framerate=25, timestamp=True, timestamp_pos=(50,50), timestamp_fontcolor=(255, 255, 255), timestamp_fontsize=1, timestamp_fontthickness=1, timestamp_format="%d/%m/%Y, %H:%M:%S"):
         self.file_name = 'default_name' # This should be replaces with a value given in self.start()
-        
-        self.camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
-        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, res_x)
-        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, res_y)
-        self.camera.set(cv2.CAP_PROP_FPS, framerate)
-        
+      
         self.framerate = framerate
         self.res_x = res_x
         self.res_y = res_y
-        
-        
-        
-        print(self.file_name)
         
         self.timestamp = timestamp
         self.timestamp_pos = timestamp_pos
@@ -59,5 +50,10 @@ class VideoRecorder:
     def start(self, file_name, file_dir):
         self.file_name = '{}/{}.h264'.format(file_dir, file_name)
 
+        self.camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.res_x)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.res_y)
+        self.camera.set(cv2.CAP_PROP_FPS, self.framerate)
+        
         video_thread = threading.Thread(target=self.record)
         video_thread.start()
